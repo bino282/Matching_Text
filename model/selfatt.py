012045cc90b7953a855c2d15ee4d6118f9356778
Nhã Nguyễn Vạn <nha282@gmail.com>
2019-02-26 08:58:36 +0700

    upfile

--- model/selfatt.py
+++ model/selfatt.py
@@ -15,7 +15,7 @@ class SELF_ATT():
     def build(self):
         seq1 = Input(name='seq1', shape=[self.config['seq1_maxlen']])
         seq2 = Input(name='seq2', shape=[self.config['seq2_maxlen']])
-        embedding = Embedding(self.git pull['vocab_size'], self.config['embed_size'], weights=[self.config['embed']], trainable = self.config['embed_trainable'])
+        embedding = Embedding(self.config['vocab_size'], self.config['embed_size'], weights=[self.config['embed']], trainable = self.config['embed_trainable'])
 
         seq1_embed = embedding(seq1)
         seq1_embed = Dropout(0.5)(seq1_embed)
